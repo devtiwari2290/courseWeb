9d25dbd96c476563787d25f76b749f3d5d13a047
Dev Tiwari <devtiwari2290@gmail.com>
2025-03-04 09:54:31 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-const express = require("express");
+import express from "express";
 const cors = require("cors");
 const app = express();
 const authRouter = require("./routes/auth.routes");
