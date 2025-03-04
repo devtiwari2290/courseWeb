96a127efbb526e5aa53172a99230d463640a7b88
Dev Tiwari <devtiwari2290@gmail.com>
2025-03-04 09:51:36 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config/config.env" });
+import dotenv from "dotenv";
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
